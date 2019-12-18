6e32cd282b81d7200fbb245161c434f9b23bcfbe
张宇海 <15736736889@163.com>
2019-12-18 17:23:53 +0800

    七牛扩展

--- application/api/validate/Qiniu.php
+++ application/api/validate/Qiniu.php
@@ -19,27 +19,9 @@ class Qiniu extends Validate
         'uri.require'    => 'uri不能为空',
     ];
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 1c66084c165e2af191ad403b9a786af1dd651098
-    // protected $scene = [
-    //     'edit'  =>  ['limit'],
-    //     'update'  =>  ['uri'],
-    // ];
-
     // ListFile 验证场景定义
     public function sceneListFiles()
     {
         return $this->only(['limit']);
     }  
-<<<<<<< HEAD
-=======
-=======
-    protected $scene = [
-        'read'  =>  ['limit'],
-        'update'  =>  ['uri'],
-    ];
->>>>>>> baf5a48637d8179d8b9881c3ba1f8ea1eb9c71cc
->>>>>>> 1c66084c165e2af191ad403b9a786af1dd651098
 }
