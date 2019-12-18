9e09c896938ac847301ac0b82734b16e97f546d3
HaI <15736736889@163.com>
2019-12-18 16:48:57 +0800

    七牛扩展升级

---
+++ application/api/validate/Qiniu.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\api\validate;
+
+use think\Validate;
+/**
+ * 生成token参数验证器
+ */
+class Qiniu extends Validate
+{
+
+    protected $rule = [
+        'bucket'       =>  'require',
+        'limit'       =>  'number|min:1',
+        'uri'       =>  'require',
+    ];
+
+    protected $message  =   [
+        'bucket.require'    => 'bucket不能为空',
+        'uri.require'    => 'uri不能为空',
+    ];
+
+    // protected $scene = [
+    //     'edit'  =>  ['limit'],
+    //     'update'  =>  ['uri'],
+    // ];
+
+    // ListFile 验证场景定义
+    public function sceneListFiles()
+    {
+        return $this->only(['limit']);
+    }  
+}
