f399b44afa4b13fa4ba2627da298a346abd3af13
张宇海 <15736736889@163.com>
2020-03-13 10:32:21 +0800

    Mar 0313

--- crond.sh
+++ crond.sh
@@ -2,7 +2,7 @@
 PATH=/usr/local/php/bin:/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin # 将php路径加入都临时变量中
 
 cd /home/wwwroot/apl  # 进入项目的根目录下，保证可以运行php think的命令
-php think Test # 执行在Test.php设定的名称
+php think Bbm # 执行在Test.php设定的名称
 
 cd /home/wwwroot/upload  # 进入文件上传目录
 rm -rf * # 清空上传的临时文件
